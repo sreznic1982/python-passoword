bc38e67d49fd9ec4a4552b5213fc6cf588785e8e
sreznic1982 <143743976+sreznic1982@users.noreply.github.com>
2023-09-28 08:34:35 -0400

    Add files via upload

---
+++ pythonpassword.py
@@ -0,0 +1,7 @@
+echo "# python-passoword" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sreznic1982/python-passoword.git
+git push -u origin main
