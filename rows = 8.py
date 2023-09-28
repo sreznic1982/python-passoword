83cd01c219dd7039f6bcafe79a014e250542946b
sreznic1982 <143743976+sreznic1982@users.noreply.github.com>
2023-09-28 08:39:23 -0400

    Add files via upload

---
+++ rows = 8.py
@@ -0,0 +1,14 @@
+score = 95
+if score < 60:
+    print("Your grade is F")
+else:
+    if score < 70:
+        print("Your grade is D")
+    else:
+        if score < 80:
+            print("Your score is C")
+        else:
+            if score < 90:
+                print("Your grade is B")
+            else:
+                print("Your grade is A")
